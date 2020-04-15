b8fc2f2ef2bf5a9ae9cb7cb25526ef2f32b6c5e5
Vaib Gadodia <positivevaib@users.noreply.github.com>
2020-04-15 17:28:54 -0400

    Add jar file and fix bugs in CleanData.scala
    - Add unique variable names to new RDDs in CleanData.scala to accomodate
    changing types.

--- CleanData.scala
+++ CleanData.scala
@@ -18,28 +18,28 @@ object CleanData {
 
   // Function to clean weather data and save to HDFS
   def cleanWeatherData(sc: SparkContext, inPath: String, outPath: String): Unit = {
-    var data = sc.textFile(inPath)
-    data = data.map(line => line.split(","))
+    val rawData = sc.textFile(inPath)
+    val splitData = rawData.map(line => line.split(","))
 
     // Filter out non-essential columns
-    val columns = data.first()
+    val columns = splitData.first()
     val indices = (columns.indexOf("\"DATE\""), columns.indexOf("\"HourlyDryBulbTemperature\""), columns.indexOf("\"HourlyPresentWeatherType\""), columns.indexOf("\"HourlyRelativeHumidity\""))
 
-    data = data.map(line => (line(indices._1), line(indices._2), line(indices._3), line(indices._4))).filter(line => line._2 != "" && !line._2.contains("s")).filter(line => line._4 != "" && !line._4.contains("*"))
+    val filterData = splitData.map(line => (line(indices._1), line(indices._2), line(indices._3), line(indices._4))).filter(line => line._2 != "" && !line._2.contains("s")).filter(line => line._4 != "" && !line._4.contains("*"))
  
     // Split weather type data into three separate columns for rain, snow and fog with binary 'yes/no' values
-    var header = data.first()
-    data = data.filter(line => line != header)
+    val header = filterData.first()
+    val contentData = filterData.filter(line => line != header)
 
-    data = data.map(line => (line._1.split("T")(0), line._1.split("T")(1), line._2, checkWeather(line._3, "RA"), checkWeather(line._3, "SN"), checkWeather(line._3, "FG"), line._4))
+    val cleanData = contentData.map(line => (line._1.split("T")(0), line._1.split("T")(1), line._2, checkWeather(line._3, "RA"), checkWeather(line._3, "SN"), checkWeather(line._3, "FG"), line._4))
 
     // Reformat data to remove redundant chars and to add back updated header
-    data = data.map(line => (line._1.substring(1, 11), line._2.substring(0, 8), line._3.substring(1, line._3.length - 1), line._4, line._5, line._6, line._7.substring(1, line._7.length - 1)))
+    var finalData = cleanData.map(line => (line._1.substring(1, 11), line._2.substring(0, 8), line._3.substring(1, line._3.length - 1), line._4, line._5, line._6, line._7.substring(1, line._7.length - 1)))
 
-    header = Array(("date", "time", "hourly  dry  bulb temperature", "rain", "snow", "fog", "hourly relative humidity"))
-    data = sc.parallelize(header) ++ data 
+    val newHeader = Array(("date", "time", "hourly  dry  bulb temperature", "rain", "snow", "fog", "hourly relative humidity"))
+    finalData = sc.parallelize(newHeader) ++ finalData 
 
-    data = data.map(line => line.toString.substring(1, line.length - 1))
+    val trimFinalData = finalData.map(line => line.toString.substring(1, line.toString.length - 1))
 
     // Save final version of cleaned data as text file
     trimFinalData.saveAsTextFile(outPath)
