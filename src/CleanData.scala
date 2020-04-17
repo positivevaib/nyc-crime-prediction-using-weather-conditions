86c1ccf16f5c56e2871e8699c09f7ad8449c9c7b
Vaib Gadodia <positivevaib@users.noreply.github.com>
2020-04-16 23:42:22 -0400

    Reformat cleaned data

--- src/CleanData.scala
+++ src/CleanData.scala
@@ -27,15 +27,14 @@ object CleanData {
 
     val filterData = splitData.map(line => (line(indices._1), line(indices._2), line(indices._3), line(indices._4))).filter(line => line._2 != "" && !line._2.contains("s")).filter(line => line._4 != "" && !line._4.contains("*"))
  
-    // Split weather type data into three separate columns for rain, snow and fog with binary 'yes/no' values
+    // Split date into year, month and day columns, time into minutes and weather type data into three separate columns for rain, snow and fog with binary 'yes/no' values
     val header = filterData.first()
     val contentData = filterData.filter(line => line != header)
 
-    val cleanData = contentData.map(line => (line._1.split("T")(0), line._1.split("T")(1), line._2, checkWeather(line._3, "RA"), checkWeather(line._3, "SN"), checkWeather(line._3, "FG"), line._4))
+    val cleanData = contentData.map(line => (line._1.split("T")(0).split('-')(0), line._1.split("T")(0).split('-')(1), line._1.split("T")(0).split('-')(2), (line._1.split("T")(1).split(':')(0).toInt * 60) + (line._1.split("T")(1).split(":")(1).toInt), ((line._2.substring(1, line._2.length - 1).toInt + 5) / 10) * 10, checkWeather(line._3, "RA"), checkWeather(line._3, "SN"), checkWeather(line._3, "FG"), ((line._4.substring(1, line._4.length - 1).toInt + 5) / 10) * 10))
 
     // Reformat data to remove redundant chars
-    var finalData = cleanData.map(line => (line._1.substring(1, 11), line._2.substring(0, 8), line._3.substring(1, line._3.length - 1), line._4, line._5, line._6, line._7.substring(1, line._7.length - 1)))
-
+    var finalData = cleanData.map(line => (line._1.substring(1, 5), line._2, line._3, line._4, line._5, line._6, line._7))
     val trimFinalData = finalData.map(line => line.toString.substring(1, line.toString.length - 1))
 
     // Save final version of cleaned data as text file
