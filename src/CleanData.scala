2286d9e1b7c6043ac7ca859e3e82a44ece6c2a88
qijinze911 <157124834@qq.com>
2020-04-17 10:14:12 +0800

    Clean Crime Number

--- src/CleanData.scala
+++ src/CleanData.scala
@@ -54,10 +54,18 @@ object CleanData {
     val cleanData = splitData.collect{case l if (l.length > 8) => List(l(1), l(2), l(8))}
 
     // Get rid of first line of Data
-    val filterData = cleanData.filter(line => line(0) != "CMPLNT_FR_DT")
+    val filterData1 = cleanData.filter(line => line(0) != "CMPLNT_FR_DT")
+    val filterData2 = filterData1.filter(line => line(2).length > 0)
+
+    // Get all the Crime Type
+    val CrimeType = filterData2.map(line => line(2)).distinct()
+    val CrimeTypeArray = CrimeType.collect
+
+    // Change Crime Type to Number
+    val CrimeNumber = filterData2.map(line => List(line(0), line(1), CrimeTypeArray.indexOf(line(2))))
 
     // Reformat Data
-    val finalData = filterData.map(line => (line(0) + "," + line(1) + "," + line(2)))
+    val finalData = CrimeNumber.map(line => (line(0) + "," + line(1) + "," + line(2)))
 
     // Save final version of cleaned data as text file
     finalData.saveAsTextFile(outPath)
