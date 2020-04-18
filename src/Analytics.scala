3d2fc4a44900e4bb0dbc63e337fff3293b771a14
Vaib Gadodia <positivevaib@users.noreply.github.com>
2020-04-18 18:49:48 -0400

    Change data format and add random forest code
    - Changed rain, snow and fog identifiers from yes/no to 1/0,
    respectively in CleanCode.scala. This helps with the random forest classifier code.
    - Added random forest classification code to Analytics.scala.

--- src/Analytics.scala
+++ src/Analytics.scala
@@ -5,6 +5,9 @@ import org.apache.spark.sql.types.IntegerType
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.Row
 import spark.implicits._
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.feature.StringIndexer
+import org.apache.spark.ml.classification.RandomForestClassifier
 
 object ProfileData {
   def main(args: Array[String]) {
@@ -14,11 +17,12 @@ object ProfileData {
     import sqlCtx._
 
     val weatherData = sc.textFile("/user/vag273/project/clean_weather_data")
-    val wSplit = weatherData.map(line => line.split(','))
+    val wSplit = weatherData.map(line => line.split(',')).map(line => (line(0) + "," + line(1) + "," + line(2) + "," + line(3), line(4) + "," + line(5) + "," + line(6) + "," + line(7) + "," + line(8))).groupByKey.map(line => line._1 + "," + line._2.toList(0)).map(line => line.split(','))
+
 
     val wHeader = Seq("wyear", "wmonth", "wday", "wminutes", "temp", "rain", "snow", "fog", "humidity")
     val wDF = wSplit.map(line => (line(0), line(1), line(2), line(3), line(4), line(5), line(6), line(7), line(8))).toDF(wHeader: _*)
-    val castWDF = wDF.select(wDF("wyear").cast(IntegerType).as("wyear"), wDF("wmonth").cast(IntegerType).as("wmonth"), wDF("wday").cast(IntegerType).as("wday"), wDF("wminutes").cast(IntegerType).as("wminutes"), wDF("temp").cast(IntegerType).as("temp"), wDF("rain"), wDF("snow"), wDF("fog"), wDF("humidity").cast(IntegerType).as("humidity"))
+    val castWDF = wDF.select(wDF("wyear").cast(IntegerType).as("wyear"), wDF("wmonth").cast(IntegerType).as("wmonth"), wDF("wday").cast(IntegerType).as("wday"), wDF("wminutes").cast(IntegerType).as("wminutes"), wDF("temp").cast(IntegerType).as("temp"), wDF("rain").cast(IntegerType).as("rain"), wDF("snow").cast(IntegerType).as("snow"), wDF("fog").cast(IntegerType).as("fog"), wDF("humidity").cast(IntegerType).as("humidity"))
   
     val crimeData = sc.textFile("/user/vag273/project/clean_crime_data")
     val cSplit = crimeData.map(line => line.split(','))
@@ -27,6 +31,20 @@ object ProfileData {
     val cDF = cSplit.map(line => (line(0), line(1), line(2), line(3), line(4))).toDF(cHeader: _*)
     val castCDF = cDF.select(cDF("cyear").cast(IntegerType).as("cyear"), cDF("cmonth").cast(IntegerType).as("cmonth"), cDF("cday").cast(IntegerType).as("cday"), cDF("cminutes").cast(IntegerType).as("cminutes"), cDF("type").cast(IntegerType).as("type")).filter("cyear >= 2006").filter("cyear <= 2017")
 
-    val joinDF = castCDF.join(castWDF, castCDF("cyear") === castWDF("wyear") && castCDF("cmonth") === castWDF("wmonth") && castCDF("cday") === castWDF("wday") && castCDF("cminutes") === castWDF("wminutes"), "inner")
+    val joinDF = castCDF.join(castWDF, castCDF("cyear") === castWDF("wyear") && castCDF("cmonth") === castWDF("wmonth") && castCDF("cday") === castWDF("wday") && castCDF("cminutes") === castWDF("wminutes"), "left").na.drop()
+
+    val cols = Array("temp", "rain", "snow", "fog", "humidity")
+    val assembler = new VectorAssembler().setInputCols(cols).setOutputCol("features")
+    val featureDF = assembler.transform(joinDF)
+
+    val indexer = new StringIndexer().setInputCol("type").setOutputCol("label")
+    val labelDF = indexer.fit(featureDF).transform(featureDF)
+
+    val seed = 5043
+    val Array(trainingData, testData) = labelDF.randomSplit(Array(0.7, 0.3), seed)
+    val randomForestClassifier = new RandomForestClassifier().setImpurity("gini").setMaxDepth(3).setNumTrees(200).setFeatureSubsetStrategy("auto").setSeed(seed)
+    val randomForestModel = randomForestClassifier.fit(trainingData)
+    
+    val predictionDF = randomForestModel.transform(testData)
   }
 }
