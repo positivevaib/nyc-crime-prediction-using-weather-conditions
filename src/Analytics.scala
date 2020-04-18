1f88ea5251d44b717170bc6f01c71f0e2a3c92f2
Vaib Gadodia <positivevaib@users.noreply.github.com>
2020-04-18 17:58:30 -0400

    Add code to join dataframes

--- src/Analytics.scala
+++ src/Analytics.scala
@@ -7,17 +7,6 @@ import org.apache.spark.sql.Row
 import spark.implicits._
 
 object ProfileData {
-  def updateCrimeStats(sqlCtx: SQLContext, df: DataFrame, row: Row, tempMat: Array[Array[Int]], humidMat: Array[Array[Int]], rainArr: Array[Int], snowArr: Array[Int], fogArr: Array[Int]): Unit = {
-    val year = row.getInt(0)
-    val month = row.getInt(1)
-    val day = row.getInt(2)
-    val minutes = row.getInt(3)
-    val crime = row.getInt(4)
-
-    df.registerTempTable("dfTable") 
-    sqlCtx.sql(s"""SELECT * FROM dfTable WHERE year = $year AND month = $month AND day = $day AND minutes = $minutes""").rdd.map(row => { tempMat(crime)(row.getInt(4)) += 1; humidMat(crime)(row.getInt(8)) += 1; rainArr(crime) += 1; snowArr(crime) += 1; fogArr(crime) += 1 })
-  }
-
   def main(args: Array[String]) {
     val sc = new SparkContext()
     val sqlCtx = new SQLContext(sc)
@@ -27,23 +16,17 @@ object ProfileData {
     val weatherData = sc.textFile("/user/vag273/project/clean_weather_data")
     val wSplit = weatherData.map(line => line.split(','))
 
-    val wHeader = Seq("year", "month", "day", "minutes", "temp", "rain", "snow", "fog", "humidity")
+    val wHeader = Seq("wyear", "wmonth", "wday", "wminutes", "temp", "rain", "snow", "fog", "humidity")
     val wDF = wSplit.map(line => (line(0), line(1), line(2), line(3), line(4), line(5), line(6), line(7), line(8))).toDF(wHeader: _*)
-    val castWDF = wDF.select(wDF("year").cast(IntegerType).as("year"), wDF("month").cast(IntegerType).as("month"), wDF("day").cast(IntegerType).as("day"), wDF("minutes").cast(IntegerType).as("minutes"), wDF("temp").cast(IntegerType).as("temp"), wDF("rain"), wDF("snow"), wDF("fog"), wDF("humidity").cast(IntegerType).as("humidity"))
+    val castWDF = wDF.select(wDF("wyear").cast(IntegerType).as("wyear"), wDF("wmonth").cast(IntegerType).as("wmonth"), wDF("wday").cast(IntegerType).as("wday"), wDF("wminutes").cast(IntegerType).as("wminutes"), wDF("temp").cast(IntegerType).as("temp"), wDF("rain"), wDF("snow"), wDF("fog"), wDF("humidity").cast(IntegerType).as("humidity"))
   
     val crimeData = sc.textFile("/user/vag273/project/clean_crime_data")
     val cSplit = crimeData.map(line => line.split(','))
 
-    val cHeader = Seq("year", "month", "day", "minutes", "type")
+    val cHeader = Seq("cyear", "cmonth", "cday", "cminutes", "type")
     val cDF = cSplit.map(line => (line(0), line(1), line(2), line(3), line(4))).toDF(cHeader: _*)
-    val castCDF = cDF.select(cDF("year").cast(IntegerType).as("year"), cDF("month").cast(IntegerType).as("month"), cDF("day").cast(IntegerType).as("day"), cDF("minutes").cast(IntegerType).as("minutes"), cDF("type").cast(IntegerType).as("type"))
+    val castCDF = cDF.select(cDF("cyear").cast(IntegerType).as("cyear"), cDF("cmonth").cast(IntegerType).as("cmonth"), cDF("cday").cast(IntegerType).as("cday"), cDF("cminutes").cast(IntegerType).as("cminutes"), cDF("type").cast(IntegerType).as("type")).filter("cyear >= 2006").filter("cyear <= 2017")
 
-    var tempMat = Array.ofDim[Int](71, 11)
-    var humidMat = Array.ofDim[Int](71, 10)
-    var rainArr = Array[Int](71)
-    var snowArr = Array[Int](71)
-    var fogArr = Array[Int](71)
-
-    castCDF.collect.foreach(row => updateCrimeStats(sqlCtx, castWDF, row, tempMat, humidMat, rainArr, snowArr, fogArr))
+    val joinDF = castCDF.join(castWDF, castCDF("cyear") === castWDF("wyear") && castCDF("cmonth") === castWDF("wmonth") && castCDF("cday") === castWDF("wday") && castCDF("cminutes") === castWDF("wminutes"), "inner")
   }
 }
