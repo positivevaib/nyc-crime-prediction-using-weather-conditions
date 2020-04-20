fe9a70ca11aa98bf4d4fb8a5fff3abe36d347b00
Vaib Gadodia <positivevaib@users.noreply.github.com>
2020-04-20 00:45:04 -0400

    Add new random forest code

--- src/Analytics.scala
+++ src/Analytics.scala
@@ -1,14 +1,16 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
+import org.apache.spark.ml.classification.RandomForestClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature.StringIndexer
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
+import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.types.IntegerType
-import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.Row
-import org.apache.spark.ml.feature.VectorAssembler
-import org.apache.spark.ml.feature.StringIndexer
-import org.apache.spark.ml.classification.RandomForestClassifier
 
-object ProfileData {
+object Analytics {
   def main(args: Array[String]) {
     val sc = new SparkContext()
     val sqlCtx = new SQLContext(sc)
@@ -32,19 +34,32 @@ object ProfileData {
     val castCDF = cDF.select(cDF("cyear").cast(IntegerType).as("cyear"), cDF("cmonth").cast(IntegerType).as("cmonth"), cDF("cday").cast(IntegerType).as("cday"), cDF("cminutes").cast(IntegerType).as("cminutes"), cDF("type").cast(IntegerType).as("type")).filter("cyear >= 2006").filter("cyear <= 2017")
 
     val joinDF = castCDF.join(castWDF, castCDF("cyear") === castWDF("wyear") && castCDF("cmonth") === castWDF("wmonth") && castCDF("cday") === castWDF("wday") && castCDF("cminutes") === castWDF("wminutes"), "left").na.drop()
+    joinDF.registerTempTable("table")
+    val trimDF = spark.sql("""SELECT * FROM table WHERE type = 3 OR type = 4 OR type = 8 OR type = 9 OR type = 11 OR type = 13 OR type = 30 OR type = 31 OR type = 34 OR type = 42 OR type = 45 OR type = 50 OR type = 54 OR type = 55 OR type = 63""")
 
     val cols = Array("temp", "rain", "snow", "fog", "humidity")
     val assembler = new VectorAssembler().setInputCols(cols).setOutputCol("features")
-    val featureDF = assembler.transform(joinDF)
-
     val indexer = new StringIndexer().setInputCol("type").setOutputCol("label")
-    val labelDF = indexer.fit(featureDF).transform(featureDF)
 
     val seed = 5043
-    val Array(trainingData, testData) = labelDF.randomSplit(Array(0.7, 0.3), seed)
-    val randomForestClassifier = new RandomForestClassifier().setImpurity("gini").setMaxDepth(3).setNumTrees(200).setFeatureSubsetStrategy("auto").setSeed(seed)
-    val randomForestModel = randomForestClassifier.fit(trainingData)
-    
-    val predictionDF = randomForestModel.transform(testData)
+    val Array(trainData, testData) = trimDF.randomSplit(Array(0.8, 0.2), seed)
+    val randomForestClassifier = new RandomForestClassifier().setFeatureSubsetStrategy("auto").setSeed(seed)
+    val stages = Array(assembler, indexer, randomForestClassifier)
+
+    val pipeline = new Pipeline().setStages(stages)
+
+    val paramGrid = new ParamGridBuilder().addGrid(randomForestClassifier.maxDepth, Array(4, 6, 8)).addGrid(randomForestClassifier.impurity, Array("entropy", "gini")).addGrid(randomForestClassifier.numTrees, Array(100, 150, 200)).build()
+
+    val evaluator = new MulticlassClassificationEvaluator().setLabelCol("label").setPredictionCol("prediction").setMetricName("accuracy")
+
+    val cv = new CrossValidator().setEstimator(pipeline).setEvaluator(evaluator).setEstimatorParamMaps(paramGrid).setNumFolds(5)
+    val cvModel = cv.fit(trainData)
+
+    val predictDF = cvModel.transform(testData)
+
+    val accuracy = evaluator.evaluate(predictDF) * 100
+    println("accuracy: " + accuracy + "%") 
+
+    cvModel.write.overwrite().save("/user/vag273/project/model")
   }
 }
