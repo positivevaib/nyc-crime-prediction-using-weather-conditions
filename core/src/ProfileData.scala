ab7e09cd33c4dd09a3bb54183eb70e8a48aac44d
Vaib Gadodia <positivevaib@users.noreply.github.com>
2020-04-26 00:07:34 -0400

    Add weather profiling code

--- core/src/ProfileData.scala
+++ core/src/ProfileData.scala
@@ -14,6 +14,35 @@ object ProfileData {
     // Print counts
     println(mode + " Data\nRaw count: " + rawCount)
     println("Clean count: " + cleanCount)
+
+    // Weather data - Count the number of records for different conditions
+    if (mode == "Weather") {
+      // Temperature
+      var tempCount = 0
+      for (i <- 0 to 100 by 10) {
+        tempCount = cleanData.map(line => ((line(4) + 5) / 10) * 10).filter(line => line == i).count
+        println(i + " deg. F count: " + tempCount)
+      }
+
+      // Humidity
+      var humidCount = 0
+      for (i <- 10 to 100 by 10) {
+        humidCount = cleanData.map(line => ((line(8) + 5) / 10) * 10).filter(line => line == i).count
+        println(i + "% rel. humidity count: " + humidCount)
+      }
+
+      // Rain
+      val rainCount = cleanData.map(line => line(5)).filter(line => line == 1).count
+      println("Rain count: " + rainCount)
+
+      // Snow 
+      val snowCount = cleanData.map(line => line(6)).filter(line => line == 1).count
+      println("Snow count: " + snowCount)
+
+      // Fog 
+      val fogCount = cleanData.map(line => line(7)).filter(line => line == 1).count
+      println("Fog count: " + fogCount)
+    }
   }
 
   def main(args: Array[String]) {
