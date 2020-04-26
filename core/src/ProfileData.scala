1e5a67ea290a4eb4d917d1b0087b67ef39d619bb
Vaib Gadodia <positivevaib@users.noreply.github.com>
2020-04-26 04:18:44 -0400

    Fix crime profiling

--- core/src/ProfileData.scala
+++ core/src/ProfileData.scala
@@ -21,55 +21,58 @@ object ProfileData {
       var tempCount = 0L
       for (i <- 0 to 100 by 10) {
         tempCount = cleanData.map(line => ((line(4).toInt + 5) / 10) * 10).filter(line => line == i).count
-        println(i + " deg. F count: " + tempCount)
+        println(i + " deg. F: " + tempCount)
       }
 
       // Humidity
       var humidCount = 0L
       for (i <- 10 to 100 by 10) {
         humidCount = cleanData.map(line => ((line(8).toInt + 5) / 10) * 10).filter(line => line == i).count
-        println(i + "% rel. humidity count: " + humidCount)
+        println(i + "% rel. humidity: " + humidCount)
       }
 
       // Rain
       val rainCount = cleanData.map(line => line(5)).filter(line => line == "1").count
-      println("Rain count: " + rainCount)
+      println("Rain: " + rainCount)
 
       // Snow 
       val snowCount = cleanData.map(line => line(6)).filter(line => line == "1").count
-      println("Snow count: " + snowCount)
+      println("Snow: " + snowCount)
 
       // Fog 
       val fogCount = cleanData.map(line => line(7)).filter(line => line == "1").count
-      println("Fog count: " + fogCount)
+      println("Fog: " + fogCount)
     }
-    // Crime Data - count number of each crime type in Raw and Clean Data
-    if(mode == "Crime"){
-    	val splitData = rawData.map(line => line.split(","))
-    	val cleanDataBefore = splitData.collect{case l if (l.length > 8) => List(l(1), l(2), l(8))}
-    	val filterData1 = cleanDataBefore.filter(line => line(0) != "CMPLNT_FR_DT")
-    	val filterData2 = filterData1.filter(line => line(2).length > 0)
-    	val CrimeType = filterData2.map(line => line(2)).distinct()
-    	val CrimeTypeSort = CrimeType.sortBy(line => line)
-    	val CrimeTypeArray = CrimeTypeSort.collect
-    	CrimeTypeArray(3) = "BURGLARY"
-    	CrimeTypeArray(9) = "FELONY ASSAULT"
-    	CrimeTypeArray(45) = "RAPE"
-    	CrimeTypeArray(11) = "ADMINISTRATIVE CODE"
-    	CrimeTypeArray(21) = "ASSAULT 3 & RELATED OFFENSES"
-    	CrimeTypeArray(62) = "NYS LAWS-UNCLASSIFIED VIOLATION"
-    	for (i <- 0 to 70){
-		val CountRawData = rawCrimeData.filter(line => (line(8) == CrimeTypeArray(i.toInt))).count
-        	val CountCleanData = cleanData.filter(line => line(4) == i.toString).count
-		println(i + " - " + CrimeTypeArray(i.toInt) + ": " + CountRawData + " | "+CountCleanData)
-    	}   
-     }
+
+    // Crime Data - Count the number of each reported crime type
+    if (mode == "Crime"){
+      val splitData = rawData.map(line => line.split(","))
+      val cleanDataBefore = splitData.collect{case l if (l.length > 8) => List(l(1), l(2), l(8))}
+      val filterData1 = cleanDataBefore.filter(line => line(0) != "CMPLNT_FR_DT")
+      val filterData2 = filterData1.filter(line => line(2).length > 0)
+      val CrimeType = filterData2.map(line => line(2)).distinct()
+      val CrimeTypeSort = CrimeType.sortBy(line => line)
+      val CrimeTypeArray = CrimeTypeSort.collect
+
+      CrimeTypeArray(3) = "BURGLARY"
+      CrimeTypeArray(9) = "FELONY ASSAULT"
+      CrimeTypeArray(45) = "RAPE"
+      CrimeTypeArray(11) = "ADMINISTRATIVE CODE"
+      CrimeTypeArray(21) = "ASSAULT 3 & RELATED OFFENSES"
+      CrimeTypeArray(62) = "NYS LAWS-UNCLASSIFIED VIOLATION"
+
+      for (i <- 0 to 70){
+        val CountCleanData = cleanData.filter(line => line(4) == i.toString).count
+		println(i + " - " + CrimeTypeArray(i.toInt) + ": " + CountCleanData)
+      }   
+    }
   }
 
   def main(args: Array[String]) {
     val sc = new SparkContext()
   
     profileData(sc, "Weather", "/user/vag273/project/weather_data", "/user/vag273/project/clean_weather_data")
+    print("\n")
     profileData(sc, "Crime", "/user/vag273/project/crime_data", "/user/vag273/project/clean_crime_data")
   }
 }
